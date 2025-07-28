302b35d4acbef623e08445527e937dd372bd315a
Duvan0417 <narvaezduvan29@gmail.com>
2025-07-28 11:37:56 -0500

    oper

---
+++ Tuplas/List.py
@@ -0,0 +1,23 @@
+#La funcion len() me permite el numero de datos de una lista
+"""
+list = ["apple","banana", "cherry"]
+print(len(list))
+"""
+#Estos son los tipos de datos para una lista
+"""
+list1 = ["apple","banana","cherry"]
+list2 = [1,2,3,4,5,6]
+list3 = [True, False, False]
+"""
+#En una misma lista pueden haber diferntes tipos de datos
+"""
+list1 = ["abc",34, True, 40, "male"]
+"""
+#En python el tipo de dato de una lista es list
+"""
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+"""
+#De la siguiente manera puedo consultar un solo elemento de una array
+mylist = ['apple','banana','cherry']
+print(mylist[1])
